e22cbc0af98bca5a475cf411844a844eeb2957de
Kyle Meadows <kyle.meadows@610ind.com>
2018-05-01 20:03:14 -0500

    update for ESLint

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import idbKeyVal from 'idb-keyval';
 
 const idb = function idb(store) {
     return {
-      load() {
-        return idbKeyVal.get(store);
-      },
+        load() {
+            return idbKeyVal.get(store);
+        },
 
-      save(state) {
-        return idbKeyVal.set(store, state);
-      }
+        save(state) {
+            return idbKeyVal.set(store, state);
+        }
     };
 };
 
